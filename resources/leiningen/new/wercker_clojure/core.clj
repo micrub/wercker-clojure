ae654eb86459d1c5e20670aecf6d3d6cccc158d8
Michael Rubanov <mrubanov@gmail.com>
2016-02-26 11:56:22 +0200

    .

--- resources/leiningen/new/wercker_clojure/core.clj
+++ resources/leiningen/new/wercker_clojure/core.clj
@@ -1,7 +1,14 @@
 (ns {{namespace}}
+  (:use org.httpkit.server)
   (:gen-class))
 
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run-server app {:port 8080
+                   :ip "0.0.0.0"
+                   :worker-name-prefix "{{namespace}}"}))
