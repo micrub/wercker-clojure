2343c1f4806cf6b2880b06ba0a66d21f3e78e897
Michael Rubanov <mrubanov@gmail.com>
2016-03-05 00:57:31 +0200

    version bump

--- resources/leiningen/new/wercker_clojure/project.clj
+++ resources/leiningen/new/wercker_clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject {{raw-name}} "0.1.0-SNAPSHOT"
+(defproject {{raw-name}} "0.1.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
